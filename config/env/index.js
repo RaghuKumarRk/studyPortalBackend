02063127302187f97c3735ec46b09b61f916c915
RaghuKumarRk <rk25raghukumar@gmail.com>
2022-06-04 11:32:52 +0530

    Remove cofig as hardcode and add it in config env

---
+++ config/env/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DB:process.env.DB || 'mongodb+srv://raghuKumar2662:2662@raghu-works.jrqtw.mongodb.net/studyPortal?retryWrites=true&w=majority',
+    PORT: process.env.PORT || '4000',
+  }
+  
+  
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Importing Modules
  */
 const constants = require('./app/common/constants');
+const Config = require('./config/env');
 const mongoose = require('mongoose');
 const Express = require('express');
 const app = new Express();
@@ -19,23 +20,21 @@ app.get("/",(req,res)=>{
 /**
  * Starting Server
  */
-const port = process.env.PORT || 4000;
-app.listen(port,()=>{
-    console.log(`App is Running Locally on Port http://localhost:${port}`)
+app.listen(Config.PORT,()=>{
+    console.log(`App is Running Locally on Port http://localhost:${Config.PORT}`)
 })
 
 
 /**
  * Establishing DataBase Connection
  */
-const DB = "mongodb+srv://raghuKumar2662:2662@raghu-works.jrqtw.mongodb.net/studyPortal?retryWrites=true&w=majority"
 const connect = ()=> {
-    console.log('DB trying to connect on ' + new Date() + ' to url' + DB)
+    console.log('DB trying to connect on ' + new Date() + ' to url' + Config.DB)
     const options = {
         keepAlive: 1,
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
-    return mongoose.connect(DB, options)
+    return mongoose.connect(Config.DB, options)
 }
 connect()
